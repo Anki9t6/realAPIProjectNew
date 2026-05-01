0cdb90d9d5fbc0dc2b1b77ca8f4c3390a6450e11
anki9t <vinaydslr6@gmail.com>
2026-05-01 15:17:18 +0530

    new things added

--- src/main/java/api/auth/TokenManager.java
+++ src/main/java/api/auth/TokenManager.java
@@ -1,5 +1,37 @@
 package api.auth;
 
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
 public class TokenManager {
 
-}
+	 private static String token;
+	    private static long expiryTime;
+
+	    public static String getToken() {
+	        if (token == null || isTokenExpired()) {
+	            generateNewToken();
+	        }
+	        return token;
+	    }
+
+	    private static void generateNewToken() {
+	        Response response = given()
+	            .contentType("application/x-www-form-urlencoded")
+	            .formParam("client_id", "client_id")
+	            .formParam("client_secret", "client_secret")
+	            .formParam("grant_type", "client_credentials")
+	        .when()
+	            .post("/oauth/token");
+
+	        token = response.jsonPath().getString("access_token");
+	        int expiresIn = response.jsonPath().getInt("expires_in");
+
+	        expiryTime = System.currentTimeMillis() + (expiresIn * 1000);
+	    }
+
+	    private static boolean isTokenExpired() {
+	    	
+	        return System.currentTimeMillis() >= expiryTime;
+	    }
+	}
